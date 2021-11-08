e358011bef4617d1f9396a6645c30a882e096c58
Loren Zeman <Loren@lorencamille.net>
2021-11-07 22:26:34 -0500

    added: images; updated: styles.css, scripts.js, index.html;

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,17 @@
 "use strict"
 
+// Expanding cards on the index page:
+/*
 const panels = document.querySelectorAll('.panel')
-
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
-
 function removeActiveClasses() {
     panels.forEach((panel) => {
         panel.classList.remove('active')
     })
 }
+*/
